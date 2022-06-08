e01e00d720ed3468ea64cc3f32b9c968f454fb04
Jfreundlich01 <jordan.freundlich@gmail.com>
2022-06-08 19:54:18 -0400

    index working

--- server.js
+++ server.js
@@ -67,8 +67,62 @@ app.get("/", (req, res) => {
   res.send("your server is running... better catch it.");
 });
 
+//Seed
+app.get('/seed', async (req, res) => {
+  const newProducts =
+    [
+      {
+        name: 'Beans',
+        description: 'A small pile of beans. Buy more beans for a big pile of beans.',
+        img: 'https://imgur.com/LEHS8h3.png',
+        price: 5,
+        qty: 99
+      }, {
+        name: 'Bones',
+        description: 'It\'s just a bag of bones.',
+        img: 'https://imgur.com/dalOqwk.png',
+        price: 25,
+        qty: 0
+      }, {
+        name: 'Bins',
+        description: 'A stack of colorful bins for your beans and bones.',
+        img: 'https://imgur.com/ptWDPO1.png',
+        price: 7000,
+        qty: 1
+      }
+    ]
+  try {
+    const seedItems = await Product.create(newProducts)
+    res.send(seedItems)
+  } catch (err) {
+    res.send(err.message)
+  }
+})
+
+//Index
+// index route
+app.get("/store", (req, res) => {
+  // find all the products
+  Product.find({})
+    // render a template after they are found
+    .then((products) => {
+      res.render("products/index.liquid", { products });
+    })
+    // send error as json if they aren't
+    .catch((error) => {
+      res.json({ error });
+    });
+});
+
+
+
+
+
+
+
 //////////////////////////////////////////////
 // Server Listener
 //////////////////////////////////////////////
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Now Listening on port ${PORT}`));
+
