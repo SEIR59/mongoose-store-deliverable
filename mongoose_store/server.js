0c0c221df01f809a919e7cb05b3aac6e66e307d6
jhimson <jhimsonrayraboypamisa@gmail.com>
2022-06-08 16:26:45 -0400

    Connected the server to the database

--- mongoose_store/server.js
+++ mongoose_store/server.js
@@ -6,15 +6,24 @@ const express = require('express'); // import express
 const morgan = require('morgan'); //import morgan
 const methodOverride = require('method-override');
 const path = require('path');
+const mongoose = require('./models/connection')
 
 //! Create our Express Application Object Bind Liquid Templating Engine
 const app = require('liquid-express-views')(express(), {
   root: [path.resolve(__dirname, 'views/')],
 });
 
+//* -------------------------------------------------------------------------- */
+//*                                 Middlewares                                */
+//* -------------------------------------------------------------------------- */
+app.use(morgan('dev')); //? logging
+app.use(methodOverride('_method')); //? override for put and delete requests from forms
+app.use(express.urlencoded({ extended: true })); //? parse urlencoded request bodies
+app.use(express.static('public')); //? serve files from public statically
+
 app.get('/', (req, res) => {
-    res.send('Hello, World!')
-})
+  res.send('Hello, World!');
+});
 
 //* -------------------------------------------------------------------------- */
 //*                               Server Listener                              */
