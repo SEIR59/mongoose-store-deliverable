09a69cb254482d57a75afb52d83914fb8efb1dd8
jhimson <jhimsonrayraboypamisa@gmail.com>
2022-06-08 17:17:35 -0400

    Product index route showing products

--- mongoose_store/controllers/productsRoute.js
+++ mongoose_store/controllers/productsRoute.js
@@ -5,10 +5,14 @@ const express = require('express');
 const Product = require('../models/productsModel');
 const Router = express.Router();
 
-
 // ! Index Route
-Router.get('/', (req, res) => {
-  res.render('products/index');
+Router.get('/', async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.render('products/index', {products});
+  } catch (error) {
+    res.send(error);
+  }
 });
 
 module.exports = Router;
