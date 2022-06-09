8e54aa8461bbe27c654003264535479122c7c3a0
doorstrawberry <kangminsoo671@gmail.com>
2022-06-08 20:35:45 -0400

    made our product schema

--- models/products.js
+++ models/products.js
@@ -1,1 +1,29 @@
 // import dependencies
+const mongoose = require("./connections.js")
+
+// pull schema and model from mongoose
+const { Schema, model } = mongoose
+
+// make product schema
+const productSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, 'Please enter a name!']
+    },
+    description: String,
+    img: String,
+    price: {
+        type: Number,
+        min: [0, 'Not a valid price']
+    },
+    qty: {
+        type: Number,
+        min: [0, 'Not a valid quantity']
+    }
+})
+
+// make product model
+const Product = model("Product", productSchema)
+
+// export product
+module.exports = Product
