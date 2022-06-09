398eccc694924e73c013b0d8935f554c230c5562
jrbreeden <jrbreeden55@gmail.com>
2022-06-09 15:32:18 -0400

    reset up database connection

---
+++ server.js
@@ -0,0 +1,12 @@
+/////////////////////////////////////////////
+// Import Our Dependencies
+/////////////////////////////////////////////
+require("dotenv").config()
+const express = require("express")
+const methodOverride = require("method-override")
+const mongoose = require("mongoose")
+const path = require("path")
+
+
+
+
