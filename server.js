f1fde94b9b11a920edeea67365b3ac2825df0537
victortan <vtan8090@gmail.com>
2022-06-08 22:56:28 -0400

    Added dependencies and middleware

---
+++ server.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+const express = require('express')
+const app = require("liquid-express-views")(express(), { root: [path.resolve(__dirname, 'views/')] })
+const methodOverride = require('method-override')
+const path = require(path)
+const mongoose = require('./models/connection')
+const StoreRouter = require('./controllers/product')
+
+app.use(methodOverride('_method'))
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'))
+
+app.use('/product', StoreRouter)
+
+app.get('/', (req, res) => {
+    res.render('index.Liquid')
+})
+
+const PORT = process.env.PORT
+app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`))
