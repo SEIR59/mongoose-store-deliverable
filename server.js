048a0257f67f084cca12d6bf31e382bd2105befd
doorstrawberry <kangminsoo671@gmail.com>
2022-06-08 20:21:53 -0400

    first route is working

---
+++ server.js
@@ -0,0 +1,22 @@
+// importing dependencies 
+require("dotenv").config()
+const express = require('express')
+const methodOverride = require("method-override")
+const path = require("path")
+
+// creating our app object
+const app = require("liquid-express-views")(express(), { root: [path.resolve(__dirname, 'views/')] })
+
+// middleware
+app.use(methodOverride("_method"))
+app.use(express.urlencoded({extended: true}))
+app.use(express.static("public"))
+
+// routes 
+app.get("/", (req, res) => {
+    res.send("route is working")
+})
+
+// server listener
+const PORT = process.env.PORT
+app.listen(PORT, () => console.log(`Now Listening on port ${PORT}`))
