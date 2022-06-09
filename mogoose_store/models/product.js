f6828d47b6b113ef84b02b4118a87257e0481e74
Wenling Yu <yuwenlingevelyn@gmail.com>
2022-06-08 20:49:59 -0400

    create schema, build up controllers, server

---
+++ mogoose_store/models/product.js
@@ -0,0 +1,29 @@
+/////////////////////////////////////////////
+// Import Our Dependencies
+/////////////////////////////////////////////
+const mongoose = require("mongoose");
+
+////////////////////////////////////////////////
+// Our Models
+////////////////////////////////////////////////
+// pull schema and model from mongoose
+const { Schema, model } = mongoose;
+
+// make product schema
+const productSchema = new Schema({
+  name: String,
+  descprition: String,
+  img: String,
+  price: {
+    type: Number,
+    min: 0,
+  },
+  qty: {
+    type: Number,
+    min: 0,
+  },
+});
+
+const Product = model("Product", productSchema);
+
+module.exports = Product;
---
+++ mogoose_store/server.js
@@ -0,0 +1,16 @@
+/////////////////////////////////////////////
+// Import Our Dependencies
+/////////////////////////////////////////////
+require("dotenv").config(); // Load ENV Variables
+const express = require("express"); // import express
+const morgan = require("morgan"); //import morgan
+const methodOverride = require("method-override");
+const mongoose = require("mongoose");
+const path = require("path");
+
+/////////////////////////////////////////////////
+// Create our Express Application Object Bind Liquid Templating Engine
+/////////////////////////////////////////////////
+const app = require("liquid-express-views")(express(), {
+  root: [path.resolve(__dirname, "views/")],
+});
