67502130f9e005e8713745c564e6ecc575facecb
christopher chapa <chapachristopher16@gmail.com>
2022-06-09 18:49:45 -0500

    set up seed file set up connection file

--- mongoose_store/server.js
+++ mongoose_store/server.js
@@ -1,14 +1,23 @@
 const express = require('express')
 const path = require('path')
 const PORT = 3000
+const middleware = require('./utils/middleware')
 const ProductRouter = require('./controllers/product')
+
+// Use liquid and direct to folder views for pages
 const app = require("liquid-express-views")(express(), {root: [path.resolve(__dirname, 'views/')]})
 
+
+// Middleware 
+middleware(app)
 
 
 // Routes to product 
 app.use('/products/', ProductRouter)
 
+
+
+// Port to listen to
 app.listen(PORT, () => {
     console.log(`on port ${PORT}`)
 })
