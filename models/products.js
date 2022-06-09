c3b5539047d28344792d0b2cec65ca73a15a63af
Yul310 <hsy3102@gmail.com>
2022-06-09 15:09:16 -0400

    Initiated

---
+++ models/products.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+// pull schema and model from mongoose
+const { Schema, model } = mongoose;
+
+// make vampire schema
+const productSchema = new Schema({
+  name: {
+      type:String,
+      required:true
+  },
+  description: String,
+  img: String,
+  price: {
+    type:Number,
+    min:1
+  },
+  qty: {
+    type:Number,
+    min:0
+  }
+ 
+});
+
+// make Vampire model
+const Product = model("Product", productSchema);
+
+///////////////////////////////////////////////////
+// Export Model
+///////////////////////////////////////////////////
+module.exports = Product;
---
+++ server.js
@@ -0,0 +1,68 @@
+/////////////////////////////////////////////
+// Import Our Dependencies
+/////////////////////////////////////////////
+require("dotenv").config(); // Load ENV Variables
+const express = require("express"); // import express
+const morgan = require("morgan"); //import morgan
+const methodOverride = require("method-override");
+const mongoose = require("mongoose");
+const path = require("path")
+const Product = require('./models/products');
+const newProducts = require('./models/seed');
+
+
+/////////////////////////////////////////////
+// Database Connection
+/////////////////////////////////////////////
+// Setup inputs for our connect function
+const DATABASE_URL = process.env.DATABASE_URL;
+const CONFIG = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+};
+
+// Establish Connection
+mongoose.connect(DATABASE_URL, CONFIG);
+
+// Events for when connection opens/disconnects/errors
+mongoose.connection
+  .on("open", () => console.log("Connected to Mongoose"))
+  .on("close", () => console.log("Disconnected from Mongoose"))
+  .on("error", (error) => console.log(error));
+
+/////////////////////////////////////////////////
+// Create our Express Application Object Bind Liquid Templating Engine
+/////////////////////////////////////////////////
+const app = require("liquid-express-views")(express(), {root: [path.resolve(__dirname, 'views/')]})
+
+/////////////////////////////////////////////////////
+// Middleware
+/////////////////////////////////////////////////////
+app.use(morgan("tiny")); //logging
+app.use(methodOverride("_method")); // override for put and delete requests from forms
+app.use(express.urlencoded({ extended: true })); // parse urlencoded request bodies
+app.use(express.static("public")); // serve files from public statically
+
+////////////////////////////////////////////
+// Routes
+////////////////////////////////////////////
+app.get("/", (req, res) => {
+  res.send("your server is running... better catch it.");
+});
+
+//////////////////////////////////////////////
+// Server Listener
+//////////////////////////////////////////////
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`Now Listening on port ${PORT}`));
+
+//Seed
+app.get('/seed', async (req, res) => {
+ 
+  try {
+    const seedItems = await Product.create(newProducts)
+    res.send(seedItems)
+  } catch (err) {
+    res.send(err.message)
+  }
+})
