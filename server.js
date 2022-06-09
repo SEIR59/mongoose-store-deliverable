fa9821949b1dee88a98d45e37d700373dc085358
jrbreeden <jrbreeden55@gmail.com>
2022-06-09 16:02:27 -0400

    added morgan

--- server.js
+++ server.js
@@ -3,9 +3,50 @@
 /////////////////////////////////////////////
 require("dotenv").config()
 const express = require("express")
+const morgan = require("morgan")
 const methodOverride = require("method-override")
-const mongoose = require("mongoose")
 const path = require("path")
+
+/////////////////////////////////////////////////
+// Create our Express Application Object Bind Liquid Templating Engine
+/////////////////////////////////////////////////
+const app = require("liquid-express-views")(express(), { root: [path.resolve(__dirname, 'views/')] })
+
+
+/////////////////////////////////////////////////////
+// Middleware
+/////////////////////////////////////////////////////
+app.use(morgan("tiny")); //logging
+app.use(methodOverride("_method")); // override for put and delete requests from forms
+app.use(express.urlencoded({ extended: true })); // parse urlencoded request bodies
+app.use(express.static("public")); // serve files from public statically
+
+
+////////////////////////////////////////////
+// Routes
+////////////////////////////////////////////
+
+app.get("/", (req, res) => {
+    res.send("your server is running")
+})
+
+//////////////////////////////////////////////
+// Server Listener
+//////////////////////////////////////////////
+const PORT = process.env.PORT
+app.listen(PORT, () => console.log(`Now listening on port ${PORT}`))
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
