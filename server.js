e8fd79bc912298aae87833438ab5011dd6d14b0a
doorstrawberry <kangminsoo671@gmail.com>
2022-06-08 20:31:07 -0400

    modified package.json

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const express = require('express')
 const methodOverride = require("method-override")
 const path = require("path")
 
+// establishing connection to our database
+const mongoose = require("./models/connections.js")
+
 // creating our app object
 const app = require("liquid-express-views")(express(), { root: [path.resolve(__dirname, 'views/')] })
 
