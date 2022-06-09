eecfda89fc6a503cafa6cb57d704c7bf62a15def
kmcaloney15 <katymcaloney@gmail.com>
2022-06-09 19:00:39 -0400

    index page is displaying all data, and product links are live

--- controllers/products.js
+++ controllers/products.js
@@ -11,7 +11,6 @@ const Product = require("../models/product.js");
 const router = express.Router();
 //change all instances of router. get etc, to router
 
-
 /////////////////////////////////////////
 // Routes - all fruit routes specifically
 /////////////////////////////////////////
@@ -19,48 +18,49 @@ const router = express.Router();
 //SEED NEEDS TO BE NEAR THE TOP
 //any time you go to this link, it will delete all data and then add in only the data listed below
 //used to test that database is working without having to create new create page, add in data and then test
-router.get('/seed', async (req, res) => {
-    const newProducts =
-      [
-        {
-          name: 'Beans',
-          description: 'A small pile of beans. Buy more beans for a big pile of beans.',
-          img: 'https://imgur.com/LEHS8h3.png',
-          price: 5,
-          qty: 99
-        }, {
-          name: 'Bones',
-          description: 'It\'s just a bag of bones.',
-          img: 'https://imgur.com/dalOqwk.png',
-          price: 25,
-          qty: 1
-        }, {
-          name: 'Bins',
-          description: 'A stack of colorful bins for your beans and bones.',
-          img: 'https://imgur.com/ptWDPO1.png',
-          price: 7000,
-          qty: 1
-        }
-      ]
-  
-    try {
-      const seedItems = await Product.create(newProducts)
-      res.send(seedItems)
-    } catch (err) {
-      res.send(err.message)
-    }
-})
+router.get("/seed", async (req, res) => {
+  const newProducts = [
+    {
+      name: "Beans",
+      description:
+        "A small pile of beans. Buy more beans for a big pile of beans.",
+      img: "https://imgur.com/LEHS8h3.png",
+      price: 5,
+      qty: 99,
+    },
+    {
+      name: "Bones",
+      description: "It's just a bag of bones.",
+      img: "https://imgur.com/dalOqwk.png",
+      price: 25,
+      qty: 1,
+    },
+    {
+      name: "Bins",
+      description: "A stack of colorful bins for your beans and bones.",
+      img: "https://imgur.com/ptWDPO1.png",
+      price: 7000,
+      qty: 1,
+    },
+  ];
+
+  try {
+    const seedItems = await Product.create(newProducts);
+    res.send(seedItems);
+  } catch (err) {
+    res.send(err.message);
+  }
+});
 
 // Index Route / The Async/Await Method
 
 // // index route / will only show the loggin in user fruits
 router.get("/", async (req, res) => {
-    //async looks for any kind of awaits - async knows it has to wait for await to finsh running before it will run it's function
-    const products = await Product.find({}); // Fruits.find({}) takes a long time to run
-    // await has it wait a second allowing Fruits.find({}) to run before it runs allowing the data to be retrived from the database
-    res.render("products/index.liquid", { products });
-  });
-
+  //async looks for any kind of awaits - async knows it has to wait for await to finsh running before it will run it's function
+  const products = await Product.find({}); // Fruits.find({}) takes a long time to run
+  // await has it wait a second allowing Fruits.find({}) to run before it runs allowing the data to be retrived from the database
+  res.render("products/index.liquid", { products });
+});
 
 //NEW ROUTE
 router.get("/new", (req, res) => {
@@ -100,21 +100,22 @@ router.post("/", (req, res) => {
 });
 
 // EDIT ROUTE
-// router.get("/:id/edit", (req, res) => {
-//   // get the id from params
-//   const id = req.params.id;
-//   // get the fruit from the database
-//   Fruit.findById(id)
-//     .then((fruit) => {
-//       // render edit page and send fruit data
-//       res.render("fruits/edit.liquid", { fruit });
-//     })
-//     // send error as json
-//     .catch((error) => {
-//       console.log(error);
-//       res.json({ error });
-//     });
-// });
+router.get("/:id/edit", (req, res) => {
+  // get the id from params
+  const id = req.params.id;
+  // get the fruit from the database
+  Product.findById(id)
+    .then((product) => {
+      //this product is the response of the line 107
+      // render edit page and send fruit data
+      res.render("products/edit.liquid", { product }); // the data that gets passed through to the liquid page
+    })
+    // send error as json
+    .catch((error) => {
+      console.log(error);
+      res.json({ error });
+    });
+});
 
 // PUT ROUTE
 //update route
@@ -135,8 +136,6 @@ router.post("/", (req, res) => {
 //       res.json({ error });
 //     });
 // });
-
-
 
 //SHOW ROUTE SHOULD ALWAYS BE NEAR TO BOTTOM TO AVOID MESS UP WITH EARLIER PAGES
 // show route
