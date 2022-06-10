9f7b4d83975dedcac767f4b017b1ef2f63e01166
jrbreeden <jrbreeden55@gmail.com>
2022-06-09 20:15:21 -0400

    set up a port

---
+++ models/products.js
@@ -0,0 +1,39 @@
+/////////////////////////////////////////////
+// Import Our Dependencies
+/////////////////////////////////////////////
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+const model = mongoose.model
+
+////////////////////////////////////////////////
+// Our Models
+////////////////////////////////////////////////
+// pull schema and model from mongoose
+// const Schema = mongoose.Schema
+// const model = mongoose.model
+// is equal to:
+
+const productsSchema = new Schema(
+    { 
+        name: {
+            type: String,
+            required: true,
+        },
+        description: String,
+        img: String,
+        price: {
+            type: Number,
+            min: 0,
+        },
+        qty: {
+            type: Number,
+            min: 0,
+        } 
+    }
+)
+
+const Product = model("Product", productsSchema)
+module.exports = Product
+
+
+
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const express = require("express")
 const morgan = require("morgan")
 const methodOverride = require("method-override")
 const path = require("path")
+const Product = require('./models/products')
 
 /////////////////////////////////////////////////
 // Create our Express Application Object Bind Liquid Templating Engine
@@ -20,15 +21,19 @@ app.use(morgan("tiny")); //logging
 app.use(methodOverride("_method")); // override for put and delete requests from forms
 app.use(express.urlencoded({ extended: true })); // parse urlencoded request bodies
 app.use(express.static("public")); // serve files from public statically
-
+app.use(require('./controllers/seed'))
 
 ////////////////////////////////////////////
 // Routes
 ////////////////////////////////////////////
 
 app.get("/", (req, res) => {
+    console.log("homePath working")
     res.send("your server is running")
 })
+
+
+
 
 //////////////////////////////////////////////
 // Server Listener
