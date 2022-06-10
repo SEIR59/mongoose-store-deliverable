cdb96780898202d248814932085a3275030930f6
jrbreeden <jrbreeden55@gmail.com>
2022-06-09 23:47:43 -0400

    show and buy working

--- controllers/product.js
+++ controllers/product.js
@@ -25,11 +25,11 @@ router.get("/", (req, res) => {
     Product.find({})
         .then((products) => {
             console.log(products)
-            res.render("index.liquid", { products });
+            res.render("index.liquid", { products })
         })
         .catch((error) => {
-            console.log(error);
-            res.json({ error });
+            console.log(error)
+            res.json({ error })
         });
 });
 
@@ -41,8 +41,8 @@ router.post("/", (req, res) => {
             res.redirect("/products")
         })
         .catch((error) => {
-            console.log(error);
-            res.json({ error });
+            console.log(error)
+            res.json({ error })
         })
 })
 
@@ -51,6 +51,31 @@ router.get("/new", (req,res) => {
     res.render("new.liquid")
 })
 
+// Buy
+router.put("/:id/buy", (req, res) => {
+    const id = req.params.id;
+    Product.updateOne({ _id: id }, { $inc: { qty: 0 }})
+        .then((products) => {
+            res.redirect(`/products/${id}`)
+        })
+        .catch((error) => {
+            console.log(error)
+            res.json({ error })
+        })
+})
+
+// Show route
+router.get("/:id", (req, res) => {
+    const id = req.params.id;
+    Product.findById(id)
+        .then((products) => {
+            res.render("show.liquid", { products })
+        })
+        .catch((error) => {
+            console.log(error)
+            res.json({ error })
+        })
+})
 
 
 module.exports = router
