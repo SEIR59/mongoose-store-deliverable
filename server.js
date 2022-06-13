764a7ef2fe137079ca524fe77f0b3253cee2ff78
Yul310 <hsy3102@gmail.com>
2022-06-13 09:52:07 -0400

    git reset node_modules

--- server.js
+++ server.js
@@ -5,30 +5,12 @@ require("dotenv").config(); // Load ENV Variables
 const express = require("express"); // import express
 const morgan = require("morgan"); //import morgan
 const methodOverride = require("method-override");
-const mongoose = require("mongoose");
+const mongoose = require('./models/connection');//still need
 const path = require("path")
 
 const ProductRouter = require("./controllers/products")
 const SeedRouter = require("./controllers/seeds")
 
-/////////////////////////////////////////////
-// Database Connection
-/////////////////////////////////////////////
-// Setup inputs for our connect function
-const DATABASE_URL = process.env.DATABASE_URL;
-const CONFIG = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-};
-
-// Establish Connection
-mongoose.connect(DATABASE_URL, CONFIG);
-
-// Events for when connection opens/disconnects/errors
-mongoose.connection
-  .on("open", () => console.log("Connected to Mongoose"))
-  .on("close", () => console.log("Disconnected from Mongoose"))
-  .on("error", (error) => console.log(error));
 
 /////////////////////////////////////////////////
 // Create our Express Application Object Bind Liquid Templating Engine
